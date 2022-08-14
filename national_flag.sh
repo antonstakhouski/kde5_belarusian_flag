bfef2848ee8e958c2a7caddc4710966292aea009
Anton Stakhouski <antonstakhouski@gmail.com>
2022-08-14 15:10:44 +0200

    Init

---
+++ national_flag.sh
@@ -0,0 +1,9 @@
+!#/bin/bash
+
+# backup national flag (red-green)
+if test ! -f /usr/share/kf5/locale/countries/by/official_flag.png
+then
+  sudo mv /usr/share/kf5/locale/countries/by/flag.png /usr/share/kf5/locale/countries/by/official_flag.png
+fi
+
+sudo cp flag.png /usr/share/kf5/locale/countries/by/flag.png
